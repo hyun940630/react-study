ecf0c7d40004b46b31b22dfedc4c556a525feda1
xtring <hh940630@gmail.com>
2019-10-13 04:48:47 +0900

    commit

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import SassComponent from './SassComponent'
+import React, { Component } from 'react';
+import SassComponent from './SassComponent';
 
 class App extends Component {
     render() {
@@ -7,8 +7,8 @@ class App extends Component {
             <div>
                 <SassComponent />
             </div>
-        )
+        );
     }
 }
 
-export default App
+export default App;
--- styling-react/src/SassComponent.js
+++ styling-react/src/SassComponent.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import './SassComponent.scss'
+import React from 'react';
+import './SassComponent.scss';
 
 const SassComponent = () => {
     return (
-        <div className='SassComponent'>
-            <div className='box red' />
-            <div className='box orange' />
-            <div className='box yellow' />
-            <div className='box green' />
-            <div className='box blue' />
-            <div className='box indigo' />
-            <div className='box violet' />
+        <div className="SassComponent">
+            <div className="box red" />
+            <div className="box orange" />
+            <div className="box yellow" />
+            <div className="box green" />
+            <div className="box blue" />
+            <div className="box indigo" />
+            <div className="box violet" />
         </div>
-    )
-}
+    );
+};
 
-export default SassComponent
+export default SassComponent;
