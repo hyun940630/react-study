4964dd285ae6dbb409bec1fbf8bc23876a85a6b4
xtring <hh940630@gmail.com>
2019-10-14 02:07:13 +0900

    git add .

--- 직박구리/App.js
+++ 직박구리/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import CSSModule from './CSSModule';
+import SassComponent from './SassComponent';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <CSSModule />
+                <SassComponent />
             </div>
         );
     }
