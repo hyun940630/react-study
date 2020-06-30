27894e1ddefabc146a335fc05b591294ffe5bdfe
String Yellow <hh940630@gmail.com>
2020-06-30 16:17:59 +0900

    add remark

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 
 const App = () => {
+  // useState 선언
   const [text, setText] = useState("initial value");
 
   const changeText = (e) => {
+    // setText()를 통해 text(state)를 업데이트
     setText(e.target.value);
   };
 
@@ -16,3 +18,10 @@ const App = () => {
 };
 
 export default App;
+
+/*
+  useState의 선언은 
+  > const [state, setState] = useState("init value");
+  
+  <input />에 text가 입력되는 즉시 <p> 태그 안의 {text}가 업데이트됩니다.
+*/
