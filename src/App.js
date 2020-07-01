cc6c332b30275588e1d8b3089313791241a041f9
String Yellow <hh940630@gmail.com>
2020-07-01 13:26:40 +0900

    useMemo 사용하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
 function App() {
   const [string, setString] = useState("");
@@ -23,11 +23,15 @@ function App() {
     return stringSum;
   };
 
+  // useMemo을 사용하여 stringList의 변화를 감지한 경우에만 sum()을 실행시켰다.
+  // useMemo의 두번째 매개변수에 감자하고자 하는 state를 넣어주면 해당 값만을 감지하여 동작한다.
+  const resultString = useMemo(() => sum(stringList), [stringList]);
+
   return (
     <div>
       <input type="text" onChange={changeInput} />
       <button onClick={insert}>문자열 추가</button>
-      {sum(stringList)}
+      {resultString}
     </div>
   );
 }
