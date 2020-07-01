4d80cc958302a42e4918de0bed9e013590b46976
String Yellow <hh940630@gmail.com>
2020-07-01 09:33:48 +0900

    useEffect - 3 componentDidMount() 처럼 동작하기

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from "react";
 function App() {
   const [text, setText] = useState("init value1");
 
+  // useEffect의 두번째 매개변수를 [](빈배열)로 넘겨주게 되면
+  // Class-type Component의 componentDidMount()와 같은 역할을 합니다.
   useEffect(() => {
     console.log("state가 변경될 때만 호출!!");
-  }, [text]); // useEffect의 두번째 매개변수를 통해 해당 state만 감지하여 useEffect를 실행합니다.
+  }, []);
 
   const changeInput = (e) => {
     setText(e.target.value);
