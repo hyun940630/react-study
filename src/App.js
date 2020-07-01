a01c28b3e0de768d5984078f82c0cdba8712a707
String Yellow <hh940630@gmail.com>
2020-07-01 10:52:02 +0900

    useContext 사용하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { createContext, useContext } from "react";
+
+const TestContext1 = createContext();
+const Testcontext2 = createContext();
+
+const UseContextExample = () => {
+  const name = useContext(TestContext1);
+  const age = useContext(Testcontext2);
+  return <div>{name + " " + age}</div>;
+};
 
 function App() {
   return (
-    <div>
-    </div>
+    <TestContext1.Provider value="Hwang Hyun">
+      <Testcontext2.Provider value="27">
+        <UseContextExample />
+      </Testcontext2.Provider>
+    </TestContext1.Provider>
   );
 }
 
