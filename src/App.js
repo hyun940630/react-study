7d83e36c6c9db8e8526ad68c47b1af193f40dbbb
String Yellow <hh940630@gmail.com>
2020-07-01 16:04:27 +0900

    useCallback 적용 전

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useMemo, useState } from "react";
 
 function App() {
+  const [string, setString] = useState("");
+  const [stringList, setStringList] = useState([]);
+
+  const change = (e) => {
+    setString(e.target.value);
+  };
+
+  const insert = () => {
+    const newList = stringList.slice();
+    newList.push(string);
+    setStringList(newList);
+  };
+
+  const sum = (list) => {
+    console.log("문자열 합치는 중!");
+    let stringSum = "";
+    for (let value of list) {
+      stringSum += value + " ";
+    }
+    return stringSum;
+  };
+
+  const result = useMemo(() => sum(stringList), [stringList]);
+
   return (
-    <div className="App">
+    <div>
+      <input type="text" onChange={change} />
+      <button onClick={insert}>문자열 추가</button>
+      {result}
     </div>
   );
 }
