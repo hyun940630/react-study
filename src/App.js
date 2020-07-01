7e0468f8f772c8e2ec38172fc24a70e89882b85d
String Yellow <hh940630@gmail.com>
2020-07-01 09:22:05 +0900

    useEffect - 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [text, setText] = useState("init value1");
+
+  // useEffect : 해당 컴포넌트가 실행 될 때마다 useEffect 함수가 실행됩니다.
+  useEffect(() => {
+    console.log("state111111가 변경될 때 마다 호출!!");
+  });
+
+  const changeInput = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <div>
+      <p>{text}</p>
+      <input onChange={changeInput} />
     </div>
   );
 }
