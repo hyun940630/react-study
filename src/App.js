6eda87c3378630b0f69e789894407addc486d011
String Yellow <hh940630@gmail.com>
2020-07-01 13:21:55 +0900

    useMemo 사용하기 전

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [string, setString] = useState("");
+  const [stringList, setStringList] = useState([]);
+
+  const changeInput = (e) => {
+    setString(e.target.value);
+  };
+
+  const insert = () => {
+    const newList = stringList.slice();
+    newList.push(string);
+    setStringList(newList);
+  };
+
+  const sum = (list) => {
+    console.log("문자열 합치는 중!");
+    let stringSum = "";
+    for (let value of list) {
+      stringSum += value + " ";
+    }
+    return stringSum;
+  };
+
+  return (
+    <div>
+      <input type="text" onChange={changeInput} />
+      <button onClick={insert}>문자열 추가</button>
+      {sum(stringList)}
+    </div>
+  );
 }
 
 export default App;
