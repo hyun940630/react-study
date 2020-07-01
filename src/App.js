438d1f6293a1f2cf94928412ae4e41b5d97f16a7
String Yellow <hh940630@gmail.com>
2020-07-01 09:40:40 +0900

    useEffect - 4 componentWillUnmount 처럼 동작하기

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import React, { useEffect, useState } from "react";
 function App() {
   const [text, setText] = useState("init value1");
 
-  // useEffect의 두번째 매개변수를 [](빈배열)로 넘겨주게 되면
-  // Class-type Component의 componentDidMount()와 같은 역할을 합니다.
+  // useEffect 함수 안에서 return을 해주면
+  // Class-type Component의 componentWillUnmount()와 같은 역할을 합니다.
   useEffect(() => {
     console.log("state가 변경될 때만 호출!!");
-  }, []);
+    return () => {
+      console.log("언마운트 시 호출!");
+    };
+  });
 
   const changeInput = (e) => {
     setText(e.target.value);
