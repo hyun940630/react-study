da2567b640178be4cff038542bcbcf688fff69f5
String Yellow <hh940630@gmail.com>
2020-07-01 09:55:37 +0900

    useEffect - 4 componentWillUnmount 처럼 동작하기(마지막 unmount에서만 실행하기)

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React, { useEffect, useState } from "react";
 function App() {
   const [text, setText] = useState("init value1");
 
-  // useEffect 함수 안에서 return을 해주면
-  // Class-type Component의 componentWillUnmount()와 같은 역할을 합니다.
+  // useEffect 함수로 컴포넌트의 마지막 unmount에서만 실행하기
+  // 두번째 매개변수에 빈배열([])을 부여합니다.
   useEffect(() => {
     console.log("state가 변경될 때만 호출!!");
     return () => {
       console.log("언마운트 시 호출!");
     };
-  });
+  }, []);
 
   const changeInput = (e) => {
     setText(e.target.value);
