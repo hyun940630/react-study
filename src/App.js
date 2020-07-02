1607e7d7ab14f1cef723bb2dd75bb3499c0d8847
String Yellow <hh940630@gmail.com>
2020-07-02 11:05:40 +0900

    useReducer 사용하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return action.value;
+    default:
+      return state;
+  }
+};
 
 function App() {
-  return <div></div>;
+  const [state, dispatch] = useReducer(reducer, "initial state");
+
+  const changeInput = (e) => {
+    dispatch({ value: e.target.value, type: "CHANGE" });
+  };
+
+  return (
+    <div>
+      <h3>{state}</h3>
+      <input type="text" value={state} onChange={changeInput} />
+    </div>
+  );
 }
 
 export default App;
