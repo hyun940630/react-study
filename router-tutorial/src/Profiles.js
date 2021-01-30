1585f3f313daae29b5e1d564a8bd48c0fc62f9c3
xtring <hh940630@gmail.com>
2021-01-31 03:37:17 +0900

    Add: NavLink - using activeStyle

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import Profile from './Profile';
 
 const Profiles = () => {
+    const activeStyle = {
+        background: 'black',
+        color: 'white'
+    }
+
     return (
         <div>
             <h3>사용자 목록:</h3>
             <ul>
-                <li><Link to="/profiles/velopert">velopert</Link></li>
-                <li><Link to="/profiles/xtring">xtring</Link></li>
+                <li><NavLink activeStyle={activeStyle} to="/profiles/velopert">velopert</NavLink></li>
+                <li><NavLink activeStyle={activeStyle} to="/profiles/xtring">xtring</NavLink></li>
             </ul>
 
             <Route 
