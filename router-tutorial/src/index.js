b1f409f47d477c60a077de9854ac6e2300f880a3
xtring <hh940630@gmail.com>
2021-01-30 23:41:19 +0900

    Add: Linking pages in App.js

--- router-tutorial/src/index.js
+++ router-tutorial/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowerRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowerRouter>
+  <BrowserRouter>
     <App />
-  </BrowerRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
