6e9acc796f5e12fefe791ca8d4796311dd747951
xtring <hh940630@gmail.com>
2021-01-31 01:27:23 +0900

    Add: Sub route

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 import { Route, Link } from 'react-router-dom';
 
 const App = () => {
@@ -11,13 +11,12 @@ const App = () => {
       <ul>
         <li><Link to="/">홈</Link></li>
         <li><Link to="/about">소개</Link></li>
-        <li><Link to="/profile/velopert">velopert 프로필</Link></li>
-        <li><Link to="/profile/xtring">xtring 프로필</Link></li>
+        <li><Link to="/profiles">프로필</Link></li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 }
