afb80d81d4823eb07a59e7b6fa35ad58a9dd5b4b
xtring <hh940630@gmail.com>
2021-02-02 01:33:47 +0900

    Add: usePromise.js

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -2,7 +2,10 @@ import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from '../lib/usePromise';
+
 import { NEWS_API_KEY } from '../constants/common';
+
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -18,34 +21,26 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const query = category === 'all' ? '' : `&category=${category}`;
-                const response = await axios.get(
-                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${NEWS_API_KEY}`
-                );
-                setArticles(response.data.articles);
-            } catch(e) {
-                console.log(e);
-            }
-            setLoading(false);
-        };
-        fetchData();
+    const [loading, response, error] = usePromise(() => {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(
+            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${NEWS_API_KEY}`
+        )
     }, [category]);
 
     if (loading) {
         return <NewsListBlock>대기 중...</NewsListBlock>
     }
 
-    if(!articles) {
+    if (!response) {
         return null;
     }
 
+    if (error) {
+        return <NewsListBlock>에러 발생!</NewsListBlock>
+    }
+
+    const { articles } = response.data;
     return (
         <NewsListBlock>
             {articles.map(article => (
