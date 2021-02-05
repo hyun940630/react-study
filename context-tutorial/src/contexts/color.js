563cc64a22ad3bbf10e6ee3857e6a19a841958f4
xtring <hh940630@gmail.com>
2021-02-05 23:30:56 +0900

    Add: useContext(using hook)

--- context-tutorial/src/contexts/color.js
+++ context-tutorial/src/contexts/color.js
@@ -1,4 +1,4 @@
-import { Children, createContext, useState } from 'react';
+import { createContext, useState } from 'react';
 
 const ColorContext = createContext({ 
     state: { color: 'black', subcolor: 'red'},
