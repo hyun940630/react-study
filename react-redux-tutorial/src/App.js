393fd7505f6147fbb1884e0702c6b7ace55895bd
xtring <hh940630@gmail.com>
2021-02-07 00:56:20 +0900

    Add: UI components

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,5 +1,14 @@
+import Counter from './components/Counter';
+import Todos from './components/Todos';
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Counter number={0} />
+      <hr />
+      <Todos />
+    </div>
+  );
 };
 
 export default App;
